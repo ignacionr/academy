cdbdff7161443070b6ae9df6478fb5128f510723
Ignacio Rodriguez <ignacionr@gmail.com>
2024-08-17 20:02:23 +0000

    Update script.js version to v19 in index.html and es.html templates

--- docs/js/script.js
+++ docs/js/script.js
@@ -16,6 +16,8 @@ function convertUTCToLocalTime(utcTimeString) {
 
 const backend = "https://hook-forwarder.ignacio-c20.workers.dev/?lang=" + base_locale;
 
+var courses = null;
+
 do_register = function(event_id) {
     fetch(backend + "&op=payment&event_id=" + event_id)
     .then(response => response.json())
@@ -24,61 +26,135 @@ do_register = function(event_id) {
     });
 };
 
-fetch(`${backend}&maxResults=15`)
+function create_course_card(key, value, lessons_container) {
+    const target_table = document.createElement("table");
+    target_table.id = key;
+    target_table.classList.add("card");
+    lessons_container.appendChild(target_table);
+    const courseInfo = value.courseInfo;
+    const events = value.events;
+    // create the row for the course
+    const course_row = document.createElement("tr");
+    // create the cell for the course title
+    const image_cell = document.createElement("th");
+    image_cell.innerHTML = courseInfo.icon;
+    course_row.appendChild(image_cell);
+    const title_cell = document.createElement("th");
+    title_cell.innerText = courseInfo.title;
+    course_row.appendChild(title_cell);
+    target_table.appendChild(course_row);
+    const description_row = document.createElement("tr");
+    const description_cell = document.createElement("td");
+    description_cell.colSpan = 2;
+    description_cell. innerText = courseInfo.description;
+    description_row.appendChild(description_cell);
+    target_table.appendChild(description_row);
+    // create the cell for the course kata
+    const kata_row = document.createElement("tr");
+    const kata_cell = document.createElement("td");
+    kata_cell.colSpan = 2;
+    kata_cell.textContent = courseInfo.kata;
+    kata_row.appendChild(kata_cell);
+    // add the course row to the table
+    target_table.appendChild(kata_row);
+    // go through each of the events of the course
+    for (const event of events) {
+        // create the row for the event
+        const event_row = document.createElement("tr");
+        // create the cell for the event start time
+        const startTime_cell = document.createElement("td");
+        startTime_cell.textContent = convertUTCToLocalTime(event.startTime);
+        event_row.appendChild(startTime_cell);
+        // an anchor to register for the event
+        const register_cell = document.createElement("td");
+        const register_link = document.createElement("a");
+        register_link.onclick = function() { do_register(event.id); };
+        register_link.classList.add("button");
+        register_link.classList.add(`schedule-${event.schedule}`);
+        register_link.innerHTML = event.register_text;
+        register_cell.appendChild(register_link);
+        event_row.appendChild(register_cell);
+        target_table.appendChild(event_row);
+    }
+    return target_table;
+}
+
+// catch hash changes
+window.addEventListener("hashchange", function() {
+    const hash = window.location.hash;
+    const course_key = hash.substring(1);
+    // find if there is already a card for the course
+    var course_card = document.getElementById(course_key);
+    if (!course_card) {
+        // if not, create the card
+        const lessons_container = document.getElementById("lessons-container");
+        course_card = create_course_card(course_key, courses[course_key], lessons_container);
+    }
+    // scroll to the course card
+    course_card.scrollIntoView({ behavior: 'smooth' });
+    // highlight the course card
+    course_card.classList.add("highlight");
+    setTimeout(function() {
+        course_card.classList.remove("highlight");
+    }, 3000);
+});
+
+function create_index_card(key, value, lessons_container) {
+    const schedule = value[0].courseInfo.schedule;
+    const color = schedule[2];
+    const target_table = document.createElement("table");
+    target_table.classList.add("card");
+    target_table.style.borderColor = color;
+    target_table.style.accentColor = color;
+    lessons_container.appendChild(target_table);
+    const schedule_row = document.createElement("tr");
+    // create the cell for the course title
+    const title_cell = document.createElement("th");
+    title_cell.colSpan = 2;
+    title_cell.style.backgroundColor = color;
+    schedule_row.appendChild(title_cell);
+    target_table.appendChild(schedule_row);
+    // go through each of the events of the course
+    for (const course of value) {
+        // create the row for the event
+        const course_row = document.createElement("tr");
+        course_row.style.borderColor = color;
+        // create the cell for the event start time
+        const title_cell = document.createElement("td");
+        title_cell.textContent = course.courseInfo.title;
+        course_row.appendChild(title_cell);
+        const link_cell = document.createElement("td");
+        const title_link = document.createElement("a");
+        title_link.classList.add("button");
+        title_link.textContent = "📚";
+        title_link.href = `${base_locale}/#${course.key}`;
+        link_cell.appendChild(title_link);
+        course_row.appendChild(link_cell);
+        target_table.appendChild(course_row);
+    }
+}
+
+fetch(`${backend}&maxResults=150`)
     .then(response => response.json())
     .then(data => {
+        courses = data;
         const lessons_container = document.getElementById("lessons-container");
-        // go through each of the properties of the data object
-        for (const [key, value] of Object.entries(data)) {
-            const target_table = document.createElement("table");
-            target_table.classList.add("card");
-            lessons_container.appendChild(target_table);
-            const courseInfo = value.courseInfo;
-            const events = value.events;
-            // create the row for the course
-            const course_row = document.createElement("tr");
-            // create the cell for the course title
-            const image_cell = document.createElement("th");
-            image_cell.innerHTML = courseInfo.icon;
-            course_row.appendChild(image_cell);
-            const title_cell = document.createElement("th");
-            title_cell.innerText = courseInfo.title;
-            course_row.appendChild(title_cell);
-            target_table.appendChild(course_row);
-            const description_row = document.createElement("tr");
-            const description_cell = document.createElement("td");
-            description_cell.colSpan = 2;
-            description_cell. innerText = courseInfo.description;
-            description_row.appendChild(description_cell);
-            target_table.appendChild(description_row);
-            // create the cell for the course kata
-            const kata_row = document.createElement("tr");
-            const kata_cell = document.createElement("td");
-            kata_cell.colSpan = 2;
-            kata_cell.textContent = courseInfo.kata;
-            kata_row.appendChild(kata_cell);
-            // add the course row to the table
-            target_table.appendChild(kata_row);
-            // go through each of the events of the course
-            for (const event of events) {
-                // create the row for the event
-                const event_row = document.createElement("tr");
-                // create the cell for the event start time
-                const startTime_cell = document.createElement("td");
-                startTime_cell.textContent = convertUTCToLocalTime(event.startTime);
-                event_row.appendChild(startTime_cell);
-                // an anchor to register for the event
-                const register_cell = document.createElement("td");
-                const register_link = document.createElement("a");
-                register_link.onclick = function() { do_register(event.id); };
-                register_link.classList.add("button");
-                register_link.classList.add(`schedule-${event.schedule}`);
-                register_link.innerHTML = event.register_text;
-                register_cell.appendChild(register_link);
-                event_row.appendChild(register_cell);
-                target_table.appendChild(event_row);
+        // aggregate the courses by schedule (level)
+        const by_schedule = Object.entries(data).reduce((acc, [key, value]) => {
+            const schedule_key = value.courseInfo.schedule[0];
+            if (!acc[schedule_key]) {
+                acc[schedule_key] = [];
             }
+            acc[schedule_key].push(Object.assign(value, { key: key }));
+            return acc;
+        }, {});
+        for (const [key, value] of Object.entries(by_schedule)) {
+            create_index_card(key, value, lessons_container);
         }
+        // go through each of the properties of the data object
+        // for (const [key, value] of Object.entries(data)) {
+        //     create_course_card(key, value, lessons_container);
+        // }
     });
 
 const suggestion_form = document.getElementById("suggestion-form");
